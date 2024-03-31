51ceec77d3499960e83c32dfe36b4234958a6d66
hemant <bavishihemang7@gmail.com>
2024-03-31 17:28:36 +0530

    complete project

---
+++ client/app/constants/index.js
@@ -0,0 +1,35 @@
+export const BASE_API_URL = process.env.BASE_API_URL;
+
+export const ROLES = {
+  Admin: 'ROLE ADMIN',
+  Member: 'ROLE MEMBER',
+  Merchant: 'ROLE MERCHANT'
+};
+
+export const CART_ITEMS = 'cart_items';
+export const CART_TOTAL = 'cart_total';
+export const CART_ID = 'cart_id';
+
+export const CART_ITEM_STATUS = {
+  Processing: 'Processing',
+  Shipped: 'Shipped',
+  Delivered: 'Delivered',
+  Cancelled: 'Cancelled',
+  Not_processed: 'Not processed'
+};
+
+export const MERCHANT_STATUS = {
+  Rejected: 'Rejected',
+  Approved: 'Approved',
+  Waiting_Approval: 'Waiting Approval'
+};
+
+export const REVIEW_STATUS = {
+  Rejected: 'Rejected',
+  Approved: 'Approved',
+  Waiting_Approval: 'Waiting Approval'
+};
+
+export const EMAIL_PROVIDER = {
+  Email: 'Email'
+};
